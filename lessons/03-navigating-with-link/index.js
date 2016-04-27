80fa1f9c6afbf43fe953650b20732bedd0ef556c
great-tiger <892873387@qq.com>
2016-04-27 17:29:15 +0800

    temp

--- lessons/03-navigating-with-link/index.js
+++ lessons/03-navigating-with-link/index.js
@@ -4,11 +4,24 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
-
+/**
+ *
+ * 这里其实就是路由配置
+ * <Router history={hashHistory}>
+ *  <Route path="/" component={App}/>
+ *  <Route path="/repos" component={Repos}/>
+ *  <Route path="/about" component={About}/>
+ * </Router>
+ *
+ * history 有三种配置方式
+ * browserHistory  /repos  /about
+ * hashHistory #/repos #/about
+ * createMemoryHistory 地址一直不变
+ */
 render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
     <Route path="/repos" component={Repos}/>
     <Route path="/about" component={About}/>
   </Router>
-), document.getElementById('app'))
+), document.getElementById('app'));
