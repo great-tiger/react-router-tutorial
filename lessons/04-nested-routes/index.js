13ff72c2b442eb39c7d718313d5e8d66efdf7017
great-tiger <892873387@qq.com>
2016-04-27 17:55:42 +0800

    temp

--- lessons/04-nested-routes/index.js
+++ lessons/04-nested-routes/index.js
@@ -4,11 +4,23 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+
+/**
+ * 以下是嵌套路由的书写方式
+ *
+ * 他具体怎么用，还要看一下modules/App.js
+ * 并结合网页效果，很好理解的
+ * 主要是这句，渲染子视图
+ */
 
 render((
   <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
+    <Route path="/" component={App} >
+      <Route path="/repos" component={Repos}>
+        <Route path="/repo" component={Repo}/>
+      </Route>
+      <Route path="/about" component={About}/>
+    </Route>
   </Router>
-), document.getElementById('app'))
+), document.getElementById('app'));
