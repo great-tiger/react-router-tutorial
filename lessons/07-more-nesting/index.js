22d6c2d99a9c2b7dd697821db5fc11f1c739a116
great-tiger <892873387@qq.com>
2016-04-28 15:31:06 +0800

    temp

--- lessons/07-more-nesting/index.js
+++ lessons/07-more-nesting/index.js
@@ -6,12 +6,26 @@ import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
 
+/**
+ * 路由嵌套
+ * 路由对应的组件就组成了一个树
+ * 比如下面的设置
+ *             App
+ *          |        |
+ *       Repos     About
+ *          |
+ *       Repo
+ * 父级组件可以通过下面的表达访问到子组件
+ * {this.props.children}
+ */
+
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/repos" component={Repos}/>
-      <Route path="/repos/:userName/:repoName" component={Repo}/>
-      <Route path="/about" component={About}/>
-    </Route>
-  </Router>
-), document.getElementById('app'))
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="/repos" component={Repos}>
+                <Route path="/repos/:userName/:repoName" component={Repo}/>
+            </Route>
+            <Route path="/about" component={About}/>
+        </Route>
+    </Router>
+), document.getElementById('app'));
