074b40789d5d6ecaddc37c66311ff7936bdc53f4
great-tiger <892873387@qq.com>
2016-04-28 16:46:44 +0800

    temp

--- lessons/09-index-links/modules/App.js
+++ lessons/09-index-links/modules/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import NavLink from './NavLink'
+import {IndexLink} from 'react-router'
+
+/**
+ * 如果想直接通过下面的代码导航到首页，你会发现是不可能的
+ * 因为他始终是active的
+ * <NavLink to="/">Home</NavLink>
+ *
+ * 一共有两种解决方法：
+ * 1、IndexLink
+ * 2、onlyActiveOnIndex
+ * <NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink>
+ */
 
 export default React.createClass({
   render() {
@@ -7,6 +19,7 @@ export default React.createClass({
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
+          <li><IndexLink to="/">Home</IndexLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
